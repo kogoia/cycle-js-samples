b81aeb4e79a17a47074585e1cf1939f3cead996b
kogoia <kogo27@gmail.com>
2017-02-09 09:36:44 +0400

    main function and effect functions

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-Rx.Observable.timer(0, 1000)
-    .map(i => `Seconds elapsed ${i}`)
-    .subscribe(text => {
+// Logic (functional)
+
+function main() {
+    return Rx.Observable.timer(0, 1000)
+        .map(i => `Seconds elapsed ${i}`)
+}
+
+// Effects iImperative)
+function DOMEffect(text$) {
+    text$.subscribe(text => {
         var container = document.querySelector("#app");
         container.textContent = text;
     });
+}
+
+function consoleLogEffect(msg$) {
+    msg$.subscribe(console.log);
+}
+
+var sink = main();
+
+DOMEffect(sink);
+consoleLogEffect(sink);
